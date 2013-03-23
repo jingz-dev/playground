2dc2958e270014004f3c4dcb120d5e692a8fca12
Matcha <x8086x@gmail.com>
2013-03-23 17:53:24 +0900

    added zoom to browser history ( really buggy )

--- spark/Mandelbrot.Set/mandelbrot.js
+++ spark/Mandelbrot.Set/mandelbrot.js
@@ -11,11 +11,17 @@ var MandelbrotSet = function(canvasWidth,canvasHeight){
 	this._y2 = -1.0;
 	// zoomParameters
 	this.fixedRatio = false;
+	// color range scale
+	this.colorscale = 1;
+	// rollback
+	this.history = [];
 }
 
 // (x1,y1)  (x2,y2) should be a rectangle 
-MandelbrotSet.prototype.setZoom = function(x1,y1,x2,y2){
+MandelbrotSet.prototype.zoomIn = function(x1,y1,x2,y2){
 	if(x1 > x2 || y1 > y2)return;
+	this.history.push([this._x1,this._y1,this._x2,this._y2]);
+	history.pushState(null,null,"");
 	if(this.fixedRatio){
 		var ratio = this._x / this._y;
 		var t;
@@ -39,8 +45,19 @@ MandelbrotSet.prototype.setZoom = function(x1,y1,x2,y2){
 	this._y1 -= y1 * ly/ this._y;
 	this._x2 = this._x1 + scaleX * lx;
 	this._y2 = this._y1 - scaleY * ly;
+	
 
-}
+
+};
+
+MandelbrotSet.prototype.zoomOut = function(){
+	if(this.history.length == 0)return;
+	var his = this.history.pop();
+	this._x1 = his[0];
+	this._y1 = his[1];
+	this._x2 = his[2];
+	this._y2 = his[3];
+};
 
 MandelbrotSet.prototype.computePixel = function(x , y) {
 	// translate x,y to c
@@ -67,21 +84,27 @@ MandelbrotSet.prototype.computePicture = function(pixels){
 	// color ( currently grayscale)
 	var c;
 	var o = 0;
+	var cMax = -1;
+	var cMin = 2;
+	var cScale = this.colorscale;
 	for(var y=0,yl=this._y; y<yl; y++){
 		for(var x=0,xl=this._x; x<xl; x++){
 			c = this.computePixel(x,y);
 			o = 4 * (xl * y + x);
 			// r
-			pixels[o]   = 0xFF * c;
+			pixels[o]   = 0xFF * c * cScale;
 			// g
-			pixels[o+1] = 0xFF * c;
+			pixels[o+1] = 0xFF * c * cScale;
 			// b
-			pixels[o+2] = 0xFF * c;
+			pixels[o+2] = 0xFF * c * cScale;
 
 			// alpha
 			pixels[o+3] = 0xFF;
-
+			if(cMax < c)cMax = c;
+			if(cMin > c)cMin = c;
 		}
 	}
+	if((cMax-cMin) != 0)
+		this.colorscale = 1 / (cMax - cMin);
 	return pixels;
 }
