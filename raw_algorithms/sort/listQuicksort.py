80248a0e5ad9f72c1ebf5914cb8ca55019eb1bea
Matcha <nekozing@gmail.com>
2013-07-26 17:27:31 +0900

    added quicksort (unfinished)

--- raw_algorithms/sort/listQuicksort.py
+++ raw_algorithms/sort/listQuicksort.py
@@ -1,40 +1,46 @@
-def partition(a, st, ed):
-	print "part", st, ed, a
-	p = st
-	q = ed
-	key = a[st]
-	while p < q:
-		while a[p] <= key:
-			p += 1
-			print "p",p
-		while a[q] > key:
-			q -= 1
-			print "q",q
-		a[p], a[q] = a[q], a[p]
-		p += 1
-		q -= 1
-	a[st], a[q] = a[q], a[st]
-	return q
+def test_partition(l,a,b):
+	pivot = l[a]
+	res = partition(l,a,b)
+	smaller = True
+	for i in range(a,b):
+		if smaller and res[i] < pivot:
+			pass
+		elif res[i] == pivot and smaller == True:
+			smaller = False
+		elif not smaller and res[i] >= pivot:
+			smaller = False
+		else:
+			return False
+	return True
 
-def testpartition():
-	lst = [1,1,2,3,4,5]
-	partition(lst, 0, len(lst) - 1)
-	print lst
+def test_partition_set():
+	cases =[]
+	#inplace_even
+	cases.append([0,1,2,3,4,5,6,7,8,9])
+	#inplace_odd
+	cases.append([0,1,2,3,4,5,6,7,8])
+	#reverse_even
+	cases.append([9,8,7,6,5,4,3,2,1,0])
+	#reverse_odd
+	cases.append([8,7,6,5,4,3,2,1,0])
+	#random_even
+	cases.append([2,7,1,7,4,3,1,4,1,5])
+	#random_odd
+	cases.append([2,7,1,7,4,3,1,4,1,5,9])
+	#duplicate_even
+	cases.append([1,1,1,1,1,1,1,1,1,1])
+	#duplicate_odd
+	cases.append([1,1,1,1,1,1,1,1,1,1,1])
+	for l in cases:
+		assert test_partition(l,0,len(l))
 
-def qsort(a, st, ed):
-	print "qsort", st, ed
-	if ed - st < 2:
-		return a
-	mid = partition(a, st, ed)
-	qsort(a, st, mid - 1)
-	qsort(a, mid + 1, ed)
-	return a
-
-def testqsort():
-	lst = [3,1,4,1,5,9,2,6,5,3,5]
-	qsort(lst, 0, len(lst) - 1)
-	print lst
+def run_tests():
+	test_partition_set()
 
 
-#testpartition()
-testqsort()
+# partition from  == a  to  < b
+def partition(l,a,b):
+	
+	pass
+
+run_tests()
