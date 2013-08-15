e65cbf51bbe949f721f85b9acf8db2f88e6645d8
Matcha <nekozing@gmail.com>
2013-08-15 17:34:38 +0900

    found a bug in recursive edit distance

--- raw_algorithms/diff/edit_distance.py
+++ raw_algorithms/diff/edit_distance.py
@@ -1,6 +1,12 @@
+import sys
 """
 edit distance algorithm ( recursive )
 """
+
+"""
+this is so wrong because of the case
+a = "a"
+b = "bbbab"
 def edit_distance(a, b, i, j):
 	if i + j == 0:
 		return diff(a, b, 0, 0)
@@ -10,16 +16,30 @@ def edit_distance(a, b, i, j):
 		return edit_distance(a, b, i-1, j) + 1
 	else:
 		return min(edit_distance(a, b, i-1, j) + 1,edit_distance(a, b, i, j-1) + 1, edit_distance(a, b, i-1, j-1) + diff(a, b, i, j))
+"""
+def edit_distance(a, b, i, j):
+	if i < 0:
+		return abs(j) + 1
+	if j < 0:
+		return abs(i) + 1
+	else:
+		return min(edit_distance(a, b, i-1, j) + 1,edit_distance(a, b, i, j-1) + 1, edit_distance(a, b, i-1, j-1) + diff(a, b, i, j))
 
 def diff(a, b, i, j):
 	return 1 if a[i] != b[j] else 0
 
 def test():
 	#test
-	a = "hakuna"
-	b = "matata"
-	#print edit_distance(a,b,len(a) - 1, len(b) - 1)
-	print edit_distance(a ,b ,len(a) - 1, len(b) - 1)
+	a = ["hakuna","a"]
+	b = ["matata","bbbab"]
+	expected = [4,4]
+	try:
+		for x,y,z in zip(a,b,expected):
+			assert(edit_distance(x, y, len(x)-1, len(y)-1) == z)
+	except Exception as e:
+		print "test failed: %s" % e
+	else:
+		print "test passed"
 
 	"""
 	e(i,j) = min(e(i-1,j), e(i,j -1) + diff(i,j))
--- raw_algorithms/diff/edit_distance_dag.py
+++ raw_algorithms/diff/edit_distance_dag.py
@@ -1,7 +1,3 @@
-
-def e(a, b, i, j, mn):
-	pass
-
 def diff(a, b, i, j):
 	return 1 if a[i] != b[j] else 0
 
@@ -16,16 +12,13 @@ def edit_distance(a, b):
 	for j in range(1, n + 1):
 		for i in range(1, m + 1):
 			mn[i][j] = min(mn[i-1][j] + 1, mn[i][j-1] + 1, mn[i-1][j-1] + diff(a, b, i-1, j-1))
-	return mn
+	return mn[i][j]
 
 def test():
-	a = [3,2,3,5,5,5]
-	b = [2,3,4,5]
-	mn = edit_distance(a, b)
-	print [0,0]+b
-	# a little bit sloppy when printing the diff matrix
-	for i in range(len(mn)):
-		print [a[i-1]] + mn[i]
+	a = "b"
+	b = "aaaba"
+	print edit_distance(a, b)
+	
 
 
 
